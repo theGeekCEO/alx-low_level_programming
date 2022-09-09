64a390c3e2068f49aff953c93c2206ce75808dec
theGeekCEO <pixxellagemedia@gmail.com>
2022-09-08 23:30:54 -0700

    Hello printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -2,10 +2,10 @@
 /**
  * Description: main - Prints out a string to stdout.
  *
- * Return: 0 if success.
+ * Return 0 if success.
  */
 int main(void)
 {
-puts("\"Programming is like building a multilingual puzzle");
+printf("with proper grammar, but the outcome is a piece of art,\n");
 return (0);
 }
