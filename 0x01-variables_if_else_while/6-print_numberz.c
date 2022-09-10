7bf86c9b67aa0ce3b9060eebed83aa2214d1bf04
theGeekCEO <pixxellagemedia@gmail.com>
2022-09-10 06:28:39 -0700

    numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - Prints single digit numbers since 0 to 9
+ * Return - Always (Success)
+ */
+int main(void)
+{
+int n;
+for (n = '0'; n <= '9'; n++)
+{
+putchar(n);
+}
+putchar('\n');
+return (0);
+}
