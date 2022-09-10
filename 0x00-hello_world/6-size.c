664aef3654ec9ac7f4dd9dcdf75f56c69e3f2ca2
theGeekCEO <pixxellagemedia@gmail.com>
2022-09-10 08:45:37 -0700

    Intel

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -6,10 +6,15 @@
  */
 int main(void)
 {
-printf("Size of a char: %d byte(s)\n" sizeof(char));
-printf("Size of an int: %d byte(s)\n" sizeof(int));
-printf("Size of a long int: %d byte(s)\n" sizeof(long int));
-printf("Size of a long long int: %d byte(s)\n" sizeof(long long int));
-printf("Size of a float: %d byte(s)\n" sizeof(float));
+int a;
+long int b;
+long long int c;
+char d;
+float f;
+printf("size of a char; %1u byte(s)\n", (unsigned long)sizeof(d));
+printf("size of an int; %1u byte(s)\n", (unsigned long)sizeof(a));
+printf("size of a long int: %1u byte(s)\n", (unsigned long)sizeof(b));
+printf("size of a long long int: %1u byte(s)\n", (unsigned long)sizeof(d));
+printf("size of a float: %1u byte(s)\n", (unsigned long)sizeof(f));
 return (0);
 }
