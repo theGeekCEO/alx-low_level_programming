13975bf5463f2d19c267b7890b8f752316864e73
theGeekCEO <pixxellagemedia@gmail.com>
2022-09-10 06:40:05 -0700

    Smile in the mirror

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - Prints the alphabet at reverse
+ * Return: Always (Sucess)
+ */
+int main(void)
+{
+char c;
+for (c = 'z'; c >= 'a'; c--)
+{
+putchar(c);
+}
+putchar('\n');
+return (0);
+}
