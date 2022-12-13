2b344d3855314b8d7af303896e1451916ecbfa3e
theGeekCEO <pixxellagemedia@gmail.com>
2022-12-12 23:39:37 -0800

    fixed a bug

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P /tmp/ https://github.com/iyidaclem/alx-low_level_programming/blob/master/0x18-dynamic_libraries/nrandom.sddo
+wget -P /tmp/ https://github.com/theGeekCEO/alx-low_level_programming/master/0x18-dynamic_libraries/nrandom.sddo
 export LD_PRELOAD=/tmp/nrandom.so
